ffb5bf2604f2e5bbf02ac749c281146fd2a9aa13
adityasemalti <adityasemalti535@gmail.com>
2026-09-22 16:30:44 +0530

    1

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL
+const API_BASE_URL = "https://borderbound-backend.onrender.com/api"
+ 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
